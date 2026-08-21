6fb88e3ec70723c1a173512e27dc442cf194b59e
Chris Gillings <chris@gillings.com>
2026-08-21 18:37:21 +1000

    Disable globbing of ALT text, add LATEST_COMIC_NUMBER tracking, add active random option

--- dist/xkcd.sh
+++ dist/xkcd.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
+# disable globbing to prevent ALT text that may contain wildcard characters from resolving in the shell
+#     (q.v. Comic 1409 ALT text "SELECT * FROM GHOSTS")
+#     (and also the test argument $1)
+set -f
+
 # Directory where we save the image and JSON file (HACS default)
 save_directory="/config/www/community/xkcd-card-ha/"
+
+# Fallback save_directory for testing or other situations
+[ -d "${save_directory}" ] || save_directory="./"
+
+# track latest comic number for a full range of random choices
+LATEST_COMIC_NUMBER_FILE="latest_comic_number.txt"
+LATEST_COMIC_NUMBER=$(cat ${save_directory}${LATEST_COMIC_NUMBER_FILE} 2> /dev/null)
+[ $LATEST_COMIC_NUMBER -eq $LATEST_COMIC_NUMBER 2>/dev/null] || LATEST_COMIC_NUMBER=3200
 
 # Determine the day of the week
 day_of_week=$(date +%u) # +%u gives a numeric representation of the day of the week (1=Monday, ..., 7=Sunday)
 
-# Define the URL based on the day of the week
-if [[ "$day_of_week" == "2" || "$day_of_week" == "4" || "$day_of_week" == "6" || "$day_of_week" == "7" ]]; then
-    # Generate a random comic number between 1 and 2000
-    random_comic=$((1 + RANDOM % 2000))
+# Test mode is triggered by a numeric argument to this shell script
+if [ ! -z "$1" ] && [ $1 -eq $1 2>/dev/null ]; then
+    # get specific numbered comic (mostly for testing the globbing issue)
+    test_comic="$1"
+    url="https://xkcd.com/${test_comic}/info.0.json"
+    explainurl="https://www.explainxkcd.com/wiki/index.php/${test_comic}:"
+
+# Define the URL based on the day of the week, or if 'random' is provided as an argument
+elif [[ "$day_of_week" == "2" || "$day_of_week" == "4" || "$day_of_week" == "6" || "$day_of_week" == "7" || "$1" == "random" ]]; then
+    # Generate a random comic number between 1 and $LATEST_COMIC_NUMBER
+    random_comic=$((1 + RANDOM % $LATEST_COMIC_NUMBER))
     url="https://xkcd.com/${random_comic}/info.0.json"
     explainurl="https://www.explainxkcd.com/wiki/index.php/${random_comic}:"
 else
@@ -51,6 +71,9 @@ json_output=$(jq -n \
 
 # Save the JSON to a file
 echo $json_output > "${save_directory}xkcd_data.json"
+
+# Update latest/highest comic number in stored file
+[ $LATEST_COMIC_NUMBER -ge $comic_number 2>/dev/null ] || echo "$comic_number" > ${save_directory}${LATEST_COMIC_NUMBER_FILE}
 
 # Output the saved paths
 echo "Image saved to: $local_image_path"
